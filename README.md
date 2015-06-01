530fae710ef19678e566e84439f1f8c482cbb688
Alvar Laigna <alvar@bind.ee>
2015-06-01 14:52:41 +0300

    Cleanup

--- README.md
+++
@@ -1,3 +0,0 @@
-# SummitToSea
-SummitToSea backend &amp; api
-
--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=summittosea',
-    'username' => 'summittosea',
-    'password' => 'MtEvhxzBULmhuAFw',
-    'charset' => 'utf8',
-];
