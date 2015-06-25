582ddcf26d56d6c0f5ca5bd195de78a8b05125a6
Janar Jürisson <janar@give.ee>
2015-06-25 22:34:37 +0300

    Registering now work trough API

--- frontend/models/SignupForm.php
+++ frontend/models/SignupForm.php
@@ -13,6 +13,8 @@ class SignupForm extends Model
     public $username;
     public $email;
     public $password;
+    public $firstname;
+    public $lastname;
 
     /**
      * @inheritdoc
@@ -20,21 +22,31 @@ class SignupForm extends Model
     public function rules()
     {
         return [
-            ['username', 'filter', 'filter' => 'trim'],
-            ['username', 'required'],
-            ['username', 'unique', 'targetClass' => '\common\models\User', 'message' => 'This username has already been taken.'],
-            ['username', 'string', 'min' => 2, 'max' => 255],
+            //['username', 'filter', 'filter' => 'trim'],
+            //['username', 'required'],
+            //['username', 'unique', 'targetClass' => '\common\models\User', 'message' => 'This username has already been taken.'],
+            //['username', 'string', 'min' => 2, 'max' => 255],
 
             ['email', 'filter', 'filter' => 'trim'],
-            ['email', 'required'],
+            //['email', 'required'],
             ['email', 'email'],
             ['email', 'unique', 'targetClass' => '\common\models\User', 'message' => 'This email address has already been taken.'],
 
-            ['password', 'required'],
-            ['password', 'string', 'min' => 6],
+            //['password', 'required'],
+            //['password', 'string', 'min' => 6],
         ];
     }
-
+    
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+      return [
+      'username' => 'E-mail',
+      ];
+    }
+    
     /**
      * Signs user up.
      *
